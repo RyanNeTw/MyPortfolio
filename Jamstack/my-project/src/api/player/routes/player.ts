4d7cfa85ca03af624e1becd2cc025223e3e6da14
RyanNeTw <ezzzerqtiryan@gmail.com>
2024-04-08 22:15:19 +0200

    fix

--- Jamstack/my-project/src/api/player/routes/player.ts
+++
@@ -1,7 +0,0 @@
-/**
- * player router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::player.player');
--- Jamstack/my-project/src/api/player/services/player.ts
+++
@@ -1,7 +0,0 @@
-/**
- * player service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::player.player');
